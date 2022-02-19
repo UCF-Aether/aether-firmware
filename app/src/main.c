b798f0540e9cdb69efbedcbda30eb74910432f50
Ian Wallace <ian.wallace2560@gmail.com>
2022-02-19 17:25:50 -0500

    main.c: Start reorganzing code to use threads.

--- app/src/main.c
+++ app/src/main.c
@@ -15,11 +15,47 @@
 #include <zephyr.h>
 #include <drivers/sensor/zmod4510.h>
 
-/***************** LoRaWAN Configuration Paramters *******************/
+/*************************** Thread Definitions *******************************/
+#define BME_STACK_SIZE		1024
+#define ZMOD_STACK_SIZE		1024
+#define PM_STACK_SIZE		1024
+#define LORA_STACK_SIZE		1024
+#define USB_STACK_SIZE		1024
+
+#define NORMAL_PRIORITY		5
+#define HIGH_PRIORITY		3
+
+K_THREAD_STACK_DEFINE(bme_stack_area, BME_STACK_SIZE);
+K_THREAD_STACK_DEFINE(zmod_stack_area, ZMOD_STACK_SIZE);
+K_THREAD_STACK_DEFINE(pm_stack_area, PM_STACK_SIZE);
+K_THREAD_STACK_DEFINE(lora_stack_area, LORA_STACK_SIZE);
+K_THREAD_STACK_DEFINE(usb_stack_area, USB_STACK_SIZE);
+
+extern void bme_entry_point(void *, void *, void *);
+extern void zmod_entry_point(void *, void *, void *);
+extern void pm_entry_point(void *, void *, void *);
+extern void lora_entry_point(void *, void *, void *);
+extern void usb_entry_point(void *, void *, void *);
+
+struct k_thread bme_thread_data;
+struct k_thread zmod_thread_data;
+struct k_thread pm_thread_data;
+struct k_thread lora_thread_data;
+struct k_thread usb_thread_data;
+
+/*************************** Sensor Configuration *****************************/
 
 /* Flags for enabling sensors */
 #define ENABLE_ZMOD
 #define ENABLE_BME
+#define ENABLE_PM
+
+/* Flags to use real or fake sensor data */
+//#define ZMOD_REAL_DATA
+//#define BME_REAL_DATA
+#define PM_REAL_DATA
+
+/******************** LoRaWAN Configuration Parameters ************************/
 
 /* Enable or disable LoRaWAN for testing purposes */
 //#define ENABLE_LORAWAN
@@ -60,22 +96,23 @@ BUILD_ASSERT(DT_NODE_HAS_STATUS(DEFAULT_RADIO_NODE, okay),
 					  0x91, 0x92}
 #endif
 
-/*********************************************************************/
-
-/********************** Other Defines ********************************/
+/**************************** Other Defines ***********************************/
 
 #define DELAY K_MSEC(10000)
+#define SLEEP K_MSEC(5000)
 #define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
 
 LOG_MODULE_REGISTER(lorawan_class_a);
 
-/*********************************************************************/
+/******************************************************************************/
 
 static void dl_callback(uint8_t port, bool data_pending,
 			int16_t rssi, int8_t snr,
 			uint8_t len, const uint8_t *data)
 {
-	LOG_INF("Port %d, Pending %d, RSSI %ddB, SNR %ddBm", port, data_pending, rssi, snr);
+	LOG_INF("Port %d, Pending %d, RSSI %ddB, SNR %ddBm", 
+			port, data_pending, rssi, snr);
+
 	if (data) {
 		LOG_HEXDUMP_INF(data, len, "Payload: ");
 	}
@@ -90,7 +127,6 @@ static void lorwan_datarate_changed(enum lorawan_datarate dr)
 }
 
 #ifndef USE_ABP
-
 int init_lorawan_otaa()
 {
 	const struct device *lora_dev;
@@ -135,9 +171,7 @@ int init_lorawan_otaa()
 
 	return ret;
 }
-
 #else
-
 int init_lorawan_abp()
 {
 	const struct device *lora_dev;
@@ -187,73 +221,27 @@ int init_lorawan_abp()
 
 	return ret;
 }
-
 #endif
 
+/************************** Thread Entry Functions ****************************/
 
-void main(void)
-{
-	int ret;
+void bme_entry_point(void *arg1, void *arg2, void *arg3) {
 
-	/* Initialize LoRaWAN if we are using it */
-	#ifdef ENABLE_LORAWAN
-
-	/* Use LoRaWAN OTAA or ABP */
-	#ifndef USE_ABP
-	ret = init_lorawan_otaa();
-	#else
-	ret = init_lorawan_abp();
-	#endif
-
-	if (ret < 0)
-		return;
-	else
-		LOG_INF("Join Success!");
-
-	#endif
-
-
-	#ifdef ENABLE_BME
+	#ifdef BME_REAL_DATA
 	/* Declare bme688 device and associated sensor values */
-	//const struct device *dev_bme = DEVICE_DT_GET(DT_INST(0, bosch_bme680));
-  const struct device *dev_bme = DEVICE_DT_GET(DT_NODELABEL(bme680));
+  	const struct device *dev_bme = DEVICE_DT_GET(DT_NODELABEL(bme680));
 	struct sensor_value temp, press, humidity, gas_res;
 
-  /* Check if the BME688 is ready */
+	/* Check if the BME688 is ready */
 	if (!device_is_ready(dev_bme)) {
 		printk("BME688 is not ready!\n");
 		return;
 	}
-  #endif
-
-  #ifdef ENABLE_ZMOD
-  /* Declare zmod4510 device and associated sensor values */
-	const struct device *dev_zmod = DEVICE_DT_GET(DT_NODELABEL(zmod4510));
-	struct sensor_value fast_aqi, o3_ppb;
-
-  /* Check if the ZMOD4510 is ready */
-	if (!device_is_ready(dev_zmod)) {
-		printk("ZMOD4510 is not ready!");
-		return;
-	}
-  #endif
-
-	#ifdef ENABLE_LORAWAN
-	LOG_INF("Sending data...");
 	#endif
-	
+
 	while (1) {
 
-		#ifdef ENABLE_ZMOD
-		/* Read data from ZMOD4510 */
-		sensor_channel_get(dev_zmod, ZMOD4510_SENSOR_CHAN_FAST_AQI, &fast_aqi);
-		sensor_channel_get(dev_zmod, ZMOD4510_SENSOR_CHAN_O3, &o3_ppb);
-		LOG_INF("fast aqi: %d", fast_aqi.val1);
-		LOG_INF("o3 (ppb): %d", o3_ppb.val1);
-		k_sleep(K_MSEC(1980));
-		#endif
-
-		#ifdef ENABLE_BME
+		#ifdef BME_REAL_DATA
 		/* Read data from BME688 */
 		sensor_sample_fetch(dev_bme);
 		sensor_channel_get(dev_bme, SENSOR_CHAN_AMBIENT_TEMP, &temp);
@@ -265,10 +253,74 @@ void main(void)
 				temp.val1, temp.val2, press.val1, press.val2,
 				humidity.val1, humidity.val2, gas_res.val1,
 				gas_res.val2);
+		#else
+		printf("T: 33.33; P: 44.44; H: 55.55; G: 4444.4444\n");
 		#endif
 
-		#ifdef ENABLE_LORAWAN
+		//k_sleep(SLEEP);
+		k_yield();
+	}
+}
 
+void zmod_entry_point(void *arg1, void *arg2, void *arg3) {
+	#ifdef ZMOD_REAL_DATA
+	int ret;
+	const struct device *dev_zmod = DEVICE_DT_GET(DT_NODELABEL(zmod4510));
+	struct sensor_value fast_aqi, o3_ppb;
+
+  	/* Check if the ZMOD4510 is ready */
+	if (!device_is_ready(dev_zmod)) {
+		printk("ZMOD4510 is not ready!");
+		return;
+	}
+
+	#endif
+
+	while (1) {
+		/* Read data from ZMOD4510 */
+
+		#ifdef ZMOD_REAL_DATA
+		sensor_channel_get(dev_zmod, ZMOD4510_SENSOR_CHAN_FAST_AQI, &fast_aqi);
+		sensor_channel_get(dev_zmod, ZMOD4510_SENSOR_CHAN_O3, &o3_ppb);
+
+		printf("fast aqi: %d", fast_aqi.val1);
+		printf("o3 (ppb): %d", o3_ppb.val1);
+
+		#else
+		
+		printf("fast aqi: %d ", 25);
+		printf("o3 (ppb): %d\n", 100);
+
+		#endif
+		k_sleep(SLEEP);
+		k_yield();
+	}
+}
+
+void pm_entry_point(void *arg1, void *arg2, void *arg3) {
+
+	while (1) {
+		printf("PM sensor sample\n");
+		//k_sleep(SLEEP);
+		k_yield();
+	}
+}
+
+void lora_entry_point(void *arg1, void *arg2, void *arg3) {
+	int ret;
+	
+	#ifdef USE_ABP
+	init_lorawan_abp();
+	#else
+	init_lorawan_otaa();
+	#endif
+
+	if (ret < 0)
+		return;
+	else
+		LOG_INF("Join Success!");
+
+	while (1) {
 		char data[] = {'h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd'};
 
 		ret = lorawan_send(2, data, sizeof(data), LORAWAN_MSG_CONFIRMED);
@@ -283,9 +335,67 @@ void main(void)
 			return;
 		}
 
-		LOG_INF("Data sent!");
-		#endif
-		
-		k_sleep(DELAY);
+		k_yield();
 	}
+}
+
+void usb_entry_point(void *arg1, void *arg2, void *arg3) {
+
+	while (1) {
+		printf("Waiting for USB commands...\n");
+		//k_sleep(SLEEP);
+		k_yield();
+	}
+}
+
+void main() 
+{
+	/* Initialize Threads */
+	k_tid_t bme_tid = k_thread_create(&bme_thread_data, bme_stack_area,
+									K_THREAD_STACK_SIZEOF(bme_stack_area),
+									bme_entry_point,
+									NULL, NULL, NULL,
+									NORMAL_PRIORITY, 0, K_NO_WAIT);
+
+	k_tid_t zmod_tid = k_thread_create(&zmod_thread_data, zmod_stack_area,
+									K_THREAD_STACK_SIZEOF(zmod_stack_area),
+									zmod_entry_point,
+									NULL, NULL, NULL,
+									NORMAL_PRIORITY, 0, K_NO_WAIT);
+
+	k_tid_t pm_tid = k_thread_create(&pm_thread_data, pm_stack_area,
+									K_THREAD_STACK_SIZEOF(pm_stack_area),
+									pm_entry_point,
+									NULL, NULL, NULL,
+									NORMAL_PRIORITY, 0, K_NO_WAIT);
+
+	k_tid_t lora_tid = k_thread_create(&lora_thread_data, lora_stack_area,
+									K_THREAD_STACK_SIZEOF(lora_stack_area),
+									lora_entry_point,
+									NULL, NULL, NULL,
+									NORMAL_PRIORITY, 0, K_NO_WAIT);
+
+	k_tid_t usb_tid = k_thread_create(&usb_thread_data, usb_stack_area,
+									K_THREAD_STACK_SIZEOF(usb_stack_area),
+									usb_entry_point,
+									NULL, NULL, NULL,
+									NORMAL_PRIORITY, 0, K_NO_WAIT);
+
+	#ifdef ENABLE_BME
+	k_thread_start(&bme_thread_data);
+	#endif
+
+	#ifdef ENABLE_ZMOD
+	k_thread_start(&zmod_thread_data);
+	#endif
+
+	#ifdef ENABLE_PM
+	k_thread_start(&pm_thread_data);
+	#endif
+
+	#ifdef ENABLE_LORAWAN
+	k_thread_start(&lora_thread_data);
+	#endif
+
+	k_thread_start(&usb_thread_data);
 }
