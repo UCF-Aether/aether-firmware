909afa91cd58ed4d15becf29a8cc6f35d7dc7951
Ian Wallace <ian.wallace2560@gmail.com>
2022-02-20 17:01:46 -0500

    cayenne.h: Define types for creating data payloads using a modified version of the CayenneLPP standard

---
+++ app/src/cayenne.h
@@ -0,0 +1,31 @@
+/*
+ * Aether Sensor Node Firmware
+ *
+ * Copyright (c) 2022 Ian Wallace <ian.wallace@knights.ucf.edu>
+ *					  Paul Wood <>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* Cayenne channel ids */
+#define CAYENNE_CHANNEL_BME     0
+#define CAYENNE_CHANNEL_ZMOD    1
+#define CAYENNE_CHANNEL_PM      2
+
+/* Cayenne data types */
+#define CAYENNE_TYPE_TEMP_ZEPHYR        0
+#define CAYENNE_TYPE_PRESSURE_ZEPHYR    1
+#define CAYENNE_TYPE_HUMIDITY_ZEPHYR    2
+#define CAYENNE_TYPE_GAS_RES_ZEPHYR     3
+#define CAYENNE_TYPE_FAST_AQI           4
+#define CAYENNE_TYPE_AQI                5
+#define CAYENNE_TYPE_O3_PPB             6
+
+/* Cayenne data type data lengths */
+#define CAYENNE_SIZE_TEMP_ZEPHYR        2
+#define CAYENNE_SIZE_PRESSURE_ZEPHYR    2
+#define CAYENNE_SIZE_HUMIDITY_ZEPHYR    2
+#define CAYENNE_SIZE_GAS_RES_ZEPHYR     2
+#define CAYENNE_SIZE_FAST_AQI           1
+#define CAYENNE_SIZE_AQI                1
+#define CAYENNE_SIZE_O3_PPB             2
