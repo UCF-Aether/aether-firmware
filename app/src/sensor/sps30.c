c80563830868633bd85ad3b0b700cb62bff5904a
Paul Wood <pmwood@pm.me>
2022-04-11 17:46:03 -0400

    SPS30 averages 5 measurements

--- app/src/sensor/sps30.c
+++ app/src/sensor/sps30.c
@@ -12,6 +12,7 @@
 LOG_MODULE_REGISTER(sps30_loop, CONFIG_SENSOR_LOG_LEVEL);
 
 #define SPS_SLEEP 15000
+#define NUM_READINGS 5
 
 #define PWR_5V_DOMAIN DT_NODELABEL(pwr_5v_domain)
 
@@ -22,6 +23,7 @@ void sps_entry_point(void *_msgq, void *arg2, void *arg3) {
   const struct device *dev_sps = DEVICE_DT_GET(DT_NODELABEL(sps30));
   struct k_msgq *msgq = (struct k_msgq *) _msgq;
   static const struct device *pwr_5v_domain = DEVICE_DT_GET(DT_NODELABEL(pwr_5v_domain));
+  float pm2_5_sum = 0, pm10_sum = 0;
 
   pm_device_runtime_enable(pwr_5v_domain);
   pm_device_runtime_enable(pwr_5v_domain);
@@ -39,32 +41,32 @@ void sps_entry_point(void *_msgq, void *arg2, void *arg3) {
     pm_device_action_run(dev_sps, PM_DEVICE_ACTION_RESUME);
 
     /* Allow time for device to turn on */
-    k_msleep(2000);
+    k_msleep(30000);
 
-    sensor_sample_fetch(dev_sps);
-    sensor_channel_get(dev_sps, SENSOR_CHAN_PM_1_0, &pm1p0);
-    sensor_channel_get(dev_sps, SENSOR_CHAN_PM_2_5, &pm2p5);
-    sensor_channel_get(dev_sps, SENSOR_CHAN_PM_10, &pm10p0);
+    pm2_5_sum = 0, pm10_sum = 0;
+
+    for (int i = 0; i < NUM_READINGS; i++) {
+      sensor_sample_fetch(dev_sps);
+      sensor_channel_get(dev_sps, SENSOR_CHAN_PM_2_5, &pm2p5);
+      sensor_channel_get(dev_sps, SENSOR_CHAN_PM_10, &pm10p0);
+
+      pm2_5_sum += (float) sensor_value_to_double(&pm2p5);
+      pm10_sum += (float) sensor_value_to_double(&pm10p0);
+
+      k_msleep(5);
+    }
 
     reading.chan = CAYENNE_CHANNEL_SPS;
 
-    reading.type = CAYENNE_TYPE_PM_1_0;
-    reading.val.f = (float) sensor_value_to_double(&pm1p0);
-    LOG_DBG("PM 1p0=%f", reading.val.f);
-    k_msgq_put(msgq, &reading, K_NO_WAIT);
-
     reading.type = CAYENNE_TYPE_PM_2_5;
-    reading.val.f = (float) sensor_value_to_double(&pm2p5);
+    reading.val.f = pm2_5_sum / NUM_READINGS;
     LOG_DBG("PM 2p5=%f", reading.val.f);
     k_msgq_put(msgq, &reading, K_NO_WAIT);
 
     reading.type = CAYENNE_TYPE_PM_10;
-    reading.val.f = (float) sensor_value_to_double(&pm10p0);
+    reading.val.f = pm10_sum / NUM_READINGS;
     LOG_DBG("PM 10p0=%f", reading.val.f);
     k_msgq_put(msgq, &reading, K_NO_WAIT);
-
-    /* Allow time for sensor measurement */
-    k_msleep(2000);
 
     pm_device_action_run(dev_sps, PM_DEVICE_ACTION_SUSPEND);
     pm_device_action_run(pwr_5v_domain, PM_DEVICE_ACTION_SUSPEND);
